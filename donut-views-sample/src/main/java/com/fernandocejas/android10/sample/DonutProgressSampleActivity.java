db5c60bb5c757810d878d5454f0f888c6334d7ce
Fernando Cejas <fcejas@gmail.com>
2014-09-23 19:19:49 +0200

    Work on sample activity.

--- donut-views-sample/src/main/java/com/fernandocejas/android10/sample/DonutProgressSampleActivity.java
+++ donut-views-sample/src/main/java/com/fernandocejas/android10/sample/DonutProgressSampleActivity.java
@@ -6,8 +6,14 @@ package com.fernandocejas.android10.sample;
 
 import android.app.Activity;
 import android.os.Bundle;
+import com.fernandocejas.android10.donut.component.DonutProgressView;
 
 public class DonutProgressSampleActivity extends Activity {
+
+  private static final int MAX_PROGRESS_ANIMATION = 300;
+
+  private DonutProgressView donutProgressViewOne;
+  private DonutProgressView donutProgressViewTwo;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -17,9 +23,37 @@ public class DonutProgressSampleActivity extends Activity {
     this.mapGUI();
   }
 
-  /**
-   * Maps the graphical user interface controls.
-   */
+  @Override protected void onResume() {
+    super.onResume();
+    this.startViewAnimation();
+  }
+
   private void mapGUI() {
+    this.donutProgressViewOne = (DonutProgressView)findViewById(R.id.v_donut_progress_one);
+    this.donutProgressViewTwo = (DonutProgressView)findViewById(R.id.v_donut_progress_two);
+  }
+
+
+  /**
+   * Start animating view
+   */
+  private void startViewAnimation() {
+    final Runnable runnable = new Runnable() {
+      public void run() {
+        for (int progress = 0; progress < MAX_PROGRESS_ANIMATION; progress++) {
+          //donutProgressViewOne.incrementProgress();
+          //donutProgressViewTwo.incrementProgress();
+          try {
+            Thread.sleep(4);
+          } catch (InterruptedException e) {
+            e.printStackTrace();
+          }
+        }
+        //donutProgressViewOne.setProgress(MAX_PROGRESS_ANIMATION);
+        //donutProgressViewTwo.setProgress(MAX_PROGRESS_ANIMATION);
+      }
+    };
+    Thread thread = new Thread(runnable);
+    thread.start();
   }
 }
